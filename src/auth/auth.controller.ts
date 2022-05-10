2016fbcfb958f2c51e8cc7de83092a3d4efb7ec8
Ifeanyichukwuwebdeve <kingifean@gmail.com>
2022-05-10 04:42:35 +0100

    Edit for db

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,18 @@
+/* eslint-disable prettier/prettier */
 import { Controller, Post } from '@nestjs/common/';
 import { AuthService } from './auth.service';
 
-@Controller('auth'),
-export class AuthController{
+@Controller('auth')
+export class AuthController {
   constructor(private authservice: AuthService) {}
 
   @Post('signup')
-  signup () {
-    return 'am signed up'
+  signup() {
+    return 'am signed up';
   }
 
   @Post('signin')
   signin() {
-    return 'AM sigined in'
+    return 'AM sigined in';
   }
 }
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,14 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { Injectable } from '@nestjs/common';
+
+@Injectable({})
+export class AuthService{
+  login () {
+    
+  }
+
+  signUp () {
+
+  }
+}
